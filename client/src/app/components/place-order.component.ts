e68f7719022d8d8677755153544f4f7a147e736e
jonahng <nyhj1998@gmail.com>
2025-03-21 15:23:47 +0800

    added forms to view 2

--- client/src/app/components/place-order.component.ts
+++ client/src/app/components/place-order.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-place-order',
@@ -6,8 +7,25 @@ import { Component } from '@angular/core';
   templateUrl: './place-order.component.html',
   styleUrl: './place-order.component.css'
 })
-export class PlaceOrderComponent {
+export class PlaceOrderComponent implements OnInit{
+
+  private fb = inject(FormBuilder)
+
+  form!: FormGroup
+
+
+  ngOnInit(): void {
+    this.form = this.createForm();
+  }
 
   // TODO: Task 3
+
+
+  private createForm(): FormGroup{
+    return this.fb.group({
+      username: this.fb.control<string>('',Validators.required),
+      password: this.fb.control<string>('',Validators.required)
+    })
+  }
 
 }
